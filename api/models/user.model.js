98bbc91310b7c0888ec955851df3b1c72e3b5d21
Cenote <himangshukalita131@gmail.com>
2024-02-09 12:22:56 +0530

    Create User Model

---
+++ api/models/user.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose"
+import { type } from "os"
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    bookedVisits: {
+      type: JSON,
+    },
+    favResidency: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+)
+
+const User = mongoose.model("User", userSchema)
+
+export default User
