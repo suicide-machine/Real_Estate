5b4d234e6f6f5b1524389540703d5a8a3d0bf150
Cenote <himangshukalita131@gmail.com>
2024-02-10 02:05:01 +0530

    Create Header Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import Header from "./components/Header"
 const App = () => {
   return (
     <BrowserRouter>
-      {/* <Header /> */}
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,17 @@
 import React from "react"
+import { Link } from "react-router-dom"
 import { FaSearch } from "react-icons/fa"
 
 const Header = () => {
   return (
     <header className="bg-slate-200 shadow-lg sticky">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-        <h1 className="font-bold text-sm sm:text-lg flex flex-wrap">
-          <span className="text-slate-500">Real</span>
-          <span className="text-slate-900">Estate</span>
-        </h1>
+        <Link to={"/"}>
+          <h1 className="font-bold text-sm sm:text-lg flex flex-wrap">
+            <span className="text-slate-500">Real</span>
+            <span className="text-slate-900">Estate</span>
+          </h1>
+        </Link>
 
         <form
           action=""
@@ -22,10 +25,20 @@ const Header = () => {
           <FaSearch className="text-slate-600" />
         </form>
 
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>SignIn</li>
+        <ul className="flex gap-4">
+          <Link to={"/"}>
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link to={"/about"}>
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
+          <Link to={"/sign-in"}>
+            <li className=" text-slate-700 hover:underline">Sign in</li>
+          </Link>
         </ul>
       </div>
     </header>
