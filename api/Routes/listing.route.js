23ab8cce276ae53a08635e8e9f78bbf1d0c64e35
Cenote <himangshukalita131@gmail.com>
2024-02-23 11:24:37 +0530

    Create delete user listing route

--- api/Routes/listing.route.js
+++ api/Routes/listing.route.js
@@ -1,9 +1,13 @@
 import express from "express"
 import { verifyToken } from "../utils/verifyUser.js"
-import { createListing } from "../Controller/listing.controller.js"
+import {
+  createListing,
+  deleteListing,
+} from "../Controller/listing.controller.js"
 
 const router = express.Router()
 
 router.post("/create", verifyToken, createListing)
+router.delete("/delete/:id", verifyToken, deleteListing)
 
 export default router
